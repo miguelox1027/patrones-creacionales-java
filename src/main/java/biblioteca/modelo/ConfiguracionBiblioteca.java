d57d12d1b0c26a94754e6baa805c92304e25dcbf
miguel angel <miguela.bocanegrav@uqvirtual.edu.co>
2026-09-19 11:42:03 -0500

    Implemento Singleton en ConfiguracionBiblioteca

--- src/main/java/biblioteca/modelo/ConfiguracionBiblioteca.java
+++ src/main/java/biblioteca/modelo/ConfiguracionBiblioteca.java
@@ -2,6 +2,8 @@ package biblioteca.modelo;
 
 public class ConfiguracionBiblioteca {
 
+    private static ConfiguracionBiblioteca instancia;
+
     private String nombre;
     private String direccion;
     private double porcentajeMulta;
@@ -10,5 +12,36 @@ public class ConfiguracionBiblioteca {
         this.nombre = "Biblioteca Universitaria";
         this.direccion = "Sin dirección";
         this.porcentajeMulta = 5.0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPorcentajeMulta() {
+        return porcentajeMulta;
+    }
+
+    public void setPorcentajeMulta(double porcentajeMulta) {
+        this.porcentajeMulta = porcentajeMulta;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public static ConfiguracionBiblioteca getInstance() {
+        if (instancia == null) {
+            instancia = new ConfiguracionBiblioteca();
+        }
+        return instancia;
     }
 }
